e2e2a668def6dc81108a95f3b356514600649612
Ori Hoch <ori@uumpa.com>
2023-07-04 11:25:30 +0300

    add recreate helm option to update instance

--- bin/update_instance.sh
+++ bin/update_instance.sh
@@ -7,6 +7,20 @@ SERVICE_ACCOUNT_KEY_FILE="${2}"
 [ "${SERVICE_ACCOUNT_KEY_FILE}" == "" ] && echo missing SERVICE_ACCOUNT_KEY_FILE && exit 1
 ! [ -e "instances/${INSTANCE_NAME}/values.yaml" ] && echo missing instance values && exit 1
 
+if [ "${RECREATE_HELM}" == "true" ]; then
+  bin/cco_exec.sh "${SERVICE_ACCOUNT_KEY_FILE}" "
+    INSTANCE_ID=\$(ckan-cloud-operator ckan instance get ${INSTANCE_NAME} | grep '^id: ' | cut -d' ' -f2)
+    echo INSTANCE_ID=\$INSTANCE_ID
+    echo deleting helm...
+    helm --tiller-namespace ckan-cloud-provider-ckan-deployment-helm delete ckan-cloud-\$INSTANCE_ID
+    echo deleting helm configmaps...
+    kubectl -n ckan-cloud-provider-ckan-deployment-helm delete configmap -l NAME=ckan-cloud-\$INSTANCE_ID
+    echo deleting ingresses...
+    kubectl -n \$INSTANCE_ID delete ingress --all
+    echo OK
+  "
+fi
+
 bin/cco_exec.sh "${SERVICE_ACCOUNT_KEY_FILE}" "
 ckan-cloud-operator ckan instance update ${INSTANCE_NAME} \
     --override-spec-file /datacity-k8s/instances/${INSTANCE_NAME}/values.yaml \
